12ec2e18eed52a79ca1a20118fced9110b080f61
Syed Ali <thsyed52@yahoo.com>
2025-03-21 18:37:04 -0400

    added a kafka producer for topic javaGuides

--- src/main/java/com/example/springboot_kafka/config/kafkaTopicConfig.java
+++ src/main/java/com/example/springboot_kafka/config/kafkaTopicConfig.java
@@ -2,12 +2,14 @@ package com.example.springboot_kafka.config;
 
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class kafkaTopicConfig {
 
+    @Bean
     public NewTopic javaGuidesTopic() {
         return TopicBuilder.name("javaGuides").build();
     }
---
+++ src/main/java/com/example/springboot_kafka/kafka/Producer.java
@@ -0,0 +1,22 @@
+package com.example.springboot_kafka.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Producer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Producer.class);
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public Producer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void send(String message) {
+        LOGGER.info(String.format("Message Sent: %s", message));
+        kafkaTemplate.send("javaGuides", message);
+    }
+}
