3fb05be6667c15181923c1de96bf9300fb49cdd5
Syed Ali <thsyed52@yahoo.com>
2025-03-24 14:47:56 -0400

    User payload and Json Consumer & Producer added

---
+++ src/main/java/com/example/springboot_kafka/kafka/JsonProducer.java
@@ -0,0 +1,5 @@
+package com.example.springboot_kafka.kafka;
+
+public class JsonProducer {
+
+}
---
+++ src/main/java/com/example/springboot_kafka/payload/User.java
@@ -0,0 +1,47 @@
+package com.example.springboot_kafka.payload;
+
+public class User {
+    private String id;
+    private String name;
+    private String email;
+
+    public User(String name, String id, String email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
